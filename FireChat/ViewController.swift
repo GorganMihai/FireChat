b73e5c6b4ecf25624666db10046933bd49da62e5
Mihai Gorgan <michele0988@ymail.com>
2022-09-21 16:57:09 +0300

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Mihai Gorgan on 21.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
